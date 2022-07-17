0bf10e89d30b387d808fee016d09702b776088d5
¨Venturin¨ <diegoventurinn@gmail.com>
2022-07-17 17:15:47 -0300

    build: added express-async-errors package

--- src/controllers/assetsController.js
+++ src/controllers/assetsController.js
@@ -1,7 +1,6 @@
 const assetsService = require('../services/assetsService');
 
 const getAssetById = async (req, res, next) => {
-  try {
     const { id } = req.params;
     const asset = await assetsService.getAssetById(id);
 
@@ -13,9 +12,6 @@ const getAssetById = async (req, res, next) => {
     }
 
     res.status(200).json(asset);
-  } catch (e) {
-    next(e);
-  }
 };
 
 module.exports = {
--- src/routers/assets.js
+++ src/routers/assets.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const assetsController = require('../controllers/assetsController');
 
 const assetsRouter = express.Router();
